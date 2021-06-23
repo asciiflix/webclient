5f0312ba2d5b2612a5a021a706a4dc771c1be8d2
grossamos <email@amosgross.com>
2021-06-23 23:24:51 +0200

    Added menu to titlebar (with exxtra cool effects)

--- src/Common/TitleBar/TitleBar.tsx
+++ src/Common/TitleBar/TitleBar.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
 import "./TitleBar.css";
 import searchIcon from "./search.svg"
-import tabIcon from "./tabs.svg"
+import TitleBarMenu from '../TitleBarMenu/TitleBarMenu';
 
 interface TitlebarProps {
     username: string
@@ -12,17 +12,16 @@ interface TitlebarProps {
 export default class TitleBar extends Component<TitlebarProps> {
     static contextType = UserContext;
 
+
     render() {
         return (
             <nav className="title-bar-main-box">
                 <div className="title-bar-content-box">
                     <Link to="/" className="title-bar-title">Asciiflix</Link>
                     <div className="title-bar-links">
-                        <Link to="/"><p className="title-bar-page">Search</p><img className="title-bar-icon" src={searchIcon}/> </Link>
-
-                        {this.props.username === "" ? <Link to="/login"><p className="title-bar-page">Login</p> <img className="title-bar-icon" src={tabIcon}/></Link> :
-                            <Link to="/logout"><p className="title-bar-page">Logout</p> {/* for {value.username} */<img className="title-bar-icon" src={tabIcon}/>}</Link>}
-                        
+                        <Link to="/" className="title-bar-page-link"><p className="title-bar-page">Search</p><img className="title-bar-icon" src={searchIcon}/> </Link>
+                        <TitleBarMenu username={this.props.username}></TitleBarMenu>
+                    
                     </div>
                 </div>
             </nav>
---
+++ src/Common/TitleBarMenu/TitleBarMenu.tsx
@@ -0,0 +1,68 @@
+import React, { Component, SyntheticEvent } from 'react'
+import { Link } from 'react-router-dom'
+import tabIcon from "./tabs.svg"
+import "../TitleBar/TitleBar.css";
+import "./TitleBarMenu.css"
+
+interface TitleBarMenuProps {
+    username: string
+}
+
+interface TitleBarMenuState {
+    showMenu: boolean
+}
+
+export default class TitleBarMenu extends Component<TitleBarMenuProps, TitleBarMenuState> {
+
+    constructor(props: TitleBarMenuProps) {
+        super(props);
+        this.state = {
+            showMenu: false
+        };
+    }
+    
+    showMenu = () => {
+        console.log("Helooooo...")
+        this.setState({
+            showMenu: true
+        });
+    }
+
+    removeMenu = () => {
+        console.log("bbyyyyyyyyyyyyyeeeeee")
+
+        this.setState({
+            showMenu: false
+        });
+    }
+
+    renderMenu = () => {
+        return (
+            <div className="title-bar-menu-action-container">
+                <div className="title-bar-menu-cover" onMouseEnter={this.removeMenu} onClick={this.removeMenu}></div>
+                <div className="title-bar-menu-container">
+                    <p className='title-bar-menu-title title-bar-menu-sub-link'>{this.props.username}</p>
+                    <Link onClick={this.removeMenu} to="/" className="title-bar-menu-sub-link">Settings</Link>
+                    <Link onClick={this.removeMenu} to="/" className="title-bar-menu-sub-link">Upload</Link>
+                    <Link onClick={this.removeMenu} to="/" className="title-bar-menu-sub-link">Profile</Link>
+                    <Link onClick={this.removeMenu} to="/logout" className="title-bar-menu-sub-link">Logout</Link>
+                </div>
+            </div>
+        ) 
+    }
+    
+    render() {
+        if (this.props.username === ""){
+            return <Link to="/login" className="title-bar-page-link"><p className="title-bar-page">Login</p> <img className="title-bar-icon" src={tabIcon}/></Link>
+        }
+        return (
+            <div>
+                {/* <Link to="/logout"> */}
+                    <p className="title-bar-page" onMouseEnter={this.showMenu} onClick={this.showMenu}>{this.props.username}</p>
+                    <img onClick={this.showMenu} className="title-bar-icon" src={tabIcon}/> 
+                {/* </Link> */}
+                    {this.state.showMenu ? this.renderMenu() : <></>}
+            </div>
+        )
+    }
+}
