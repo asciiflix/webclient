c2542924a5575425de681523516dda665f97c3a5
grossamos <email@amosgross.com>
2021-06-27 23:29:56 +0200

    more removal of old stuff

--- src/Pages/LoginPage/LoginPage.tsx
+++ src/Pages/LoginPage/LoginPage.tsx
@@ -2,14 +2,11 @@ import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
 import { JwtConext } from "../../Common/JwtContext/JwtContext";
 import UserLogin from "../../Components/Login/UserLogin";
-import { EMPTY_USER_CONTEXT, UserContext } from "../../UserContext";
 
 
 export default class LoginPage extends Component {
-    static contextType = UserContext;
-
-    getUserState = () => {
-        if (this.context === EMPTY_USER_CONTEXT || this.context.jwtToken === "") {
+    getUserState = (jwtToken: string) => {
+        if (jwtToken === "") {
             return false;
         } else {
             return true;
@@ -18,16 +15,16 @@ export default class LoginPage extends Component {
 
     render() {
         return (
-            <div className="login-container">
-                {this.getUserState() ? <Redirect to="/"></Redirect> :
-                    <br></br>
-                }
-                <JwtConext.Consumer>
+            <JwtConext.Consumer>
                 {({jwtUserInfo, changeJwt}) => 
-                    <UserLogin isLoggedIn={this.getUserState()} changeJwt={changeJwt}></UserLogin>
-                    }
-                </JwtConext.Consumer>
-            </div>
+                    <div className="login-container">
+                        {this.getUserState(jwtUserInfo.jwtToken) ? <Redirect to="/"></Redirect> :
+                            <br></br>
+                        }
+                        <UserLogin isLoggedIn={this.getUserState(jwtUserInfo.jwtToken)} changeJwt={changeJwt}></UserLogin>
+                    </div>
+                }
+            </JwtConext.Consumer>
         )
     }
 }
