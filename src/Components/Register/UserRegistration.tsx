0e795d932ad72fe6abec521736989152e2fd5445
Lukas <lukas@leucali.net>
2021-07-12 19:28:01 +0200

    Added Password Repeat on Registation

--- src/Components/Register/UserRegistration.tsx
+++ src/Components/Register/UserRegistration.tsx
@@ -7,6 +7,7 @@ interface RegisterInformation {
     username: string
     email: string
     password: string
+    password_r: string
 }
 
 interface Status {
@@ -14,7 +15,8 @@ interface Status {
 }
 
 interface StatusState {
-    isRegistered: boolean
+    isRegistered: boolean,
+    wrongPW: boolean
 }
 
 export default class UserRegistration extends Component<Status, StatusState> {
@@ -22,20 +24,27 @@ export default class UserRegistration extends Component<Status, StatusState> {
     register: RegisterInformation = {
         username: "",
         email: "",
-        password: ""
+        password: "",
+        password_r: ""
     };
 
     constructor(props: StatusState) {
         super(props);
         this.state = {
-            isRegistered: props.isRegistered
+            isRegistered: props.isRegistered,
+            wrongPW: false
         };
     }
 
 
     submit_register = (e: SyntheticEvent) => {
         e.preventDefault();
-        this.registerCallApi();
+        if (this.register.password !== this.register.password_r) {
+            this.setState({ wrongPW: true });
+        } else {
+            this.setState({ wrongPW: false });
+            this.registerCallApi();
+        }
     }
 
 
@@ -65,10 +74,11 @@ export default class UserRegistration extends Component<Status, StatusState> {
     render() {
         return (
             <div className="login-form-container">
-                {this.state.isRegistered ? <Redirect to="/login"></Redirect> :                    
-                    <form onSubmit={this.submit_register}>                       
+                {this.state.isRegistered ? <Redirect to="/login"></Redirect> :
+                    <form onSubmit={this.submit_register}>
                         <div className="empty-div">
                             <h1 className="form-title-text">Register</h1>
+                            {this.state.wrongPW ? <p className="login-form-failed-login">New Password does not match</p> : <></>}
                             <label>Username</label>
                             <input className="form-input" type="name" placeholder="Username" required onChange={e => this.register.username = e.target.value}></input>
 
@@ -77,6 +87,9 @@ export default class UserRegistration extends Component<Status, StatusState> {
 
                             <label>Password</label>
                             <input className="form-input" type="password" placeholder="Password" required onChange={e => this.register.password = e.target.value}></input>
+
+                            <label>Repeat Password</label>
+                            <input className="form-input" type="password" placeholder="Password" required onChange={e => this.register.password_r = e.target.value}></input>
 
                             <button className="login-button" type="submit">Register</button>
                         </div>
